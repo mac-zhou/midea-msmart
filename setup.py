1d527f18b1fead0371527926554bd54a32205494
NeoAcheron <master@neoacheron.com>
2018-12-02 00:48:52 +0200

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='midea',  
+     version='0.1',
+     author="NeoAcheron",
+     author_email="master@neoacheron.com",
+     description="A library to control Midea appliances via the cloud API",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/NeoAcheron/midea-ac-py",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
