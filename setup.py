9f2079533be45467e3534321b0d6ccfe3e28707e
NeoAcheron <master@neoacheron.com>
2018-12-06 21:31:50 +0200

    [ci skip] Version Changed from 0.1.0 -> 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='midea',  
-     version='0.1.0',
+     version='0.1.1',
      author="NeoAcheron",
      author_email="master@neoacheron.com",
      description="A library to control Midea appliances via the cloud API",
