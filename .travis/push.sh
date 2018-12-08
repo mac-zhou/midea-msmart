a6cd90e359a32103535fb73fd73f5a89e7baeeac
NeoAcheron <master@neoacheron.com>
2018-12-08 20:10:23 +0200

    [Travis] I give up... Will try something else later.

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,14 +6,12 @@ bumpversion patch --no-tag --allow-dirty --no-commit --list > .temp
 CURRENT_VERSION=`cat .temp | grep current_version | sed -r s,"^.*=",,`
 NEW_VERSION=`cat .temp | grep new_version | sed -r s,"^.*=",,`
 
-git tag "v${NEW_VERSION}"
-
 git add .bumpversion.cfg
 git add custom_components.json
 git add midea.py
 git add setup.py
+git add midea/*.py
 
 git commit -m "[ci skip] Version Changed from ${CURRENT_VERSION} -> ${NEW_VERSION}"
+git tag "v${NEW_VERSION}"
 git remote add build https://${GH_TOKEN}@github.com/NeoAcheron/midea-ac-py.git > /dev/null 2>&1
-git branch -u build/master
-git push --quiet --tags build
