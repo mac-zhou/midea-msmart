6281143f68aa7fd8bf835876f74519f32d058c7d
NeoAcheron <master@neoacheron.com>
2018-12-08 19:48:39 +0200

    Making Travis happy

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+echo Currently on branch `git branch`
 
 bumpversion patch --no-tag --allow-dirty --no-commit --list > .temp
 CURRENT_VERSION=`cat .temp | grep current_version | sed -r s,"^.*=",,`
@@ -16,4 +15,5 @@ git add setup.py
 
 git commit -m "[ci skip] Version Changed from ${CURRENT_VERSION} -> ${NEW_VERSION}"
 git remote add build https://${GH_TOKEN}@github.com/NeoAcheron/midea-ac-py.git > /dev/null 2>&1
-git push --quiet --tags --set-upstream build master 
+git branch -u build/master
+git push --quiet --tags
