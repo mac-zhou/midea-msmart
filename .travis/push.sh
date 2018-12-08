cfe64a2f936749f657e395d8889c55e5112c3312
NeoAcheron <master@neoacheron.com>
2018-12-08 18:03:46 +0200

    Added tags to the travis script

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,11 +7,13 @@ bumpversion patch --no-tag --allow-dirty --no-commit --list > .temp
 CURRENT_VERSION=`cat .temp | grep current_version | sed -r s,"^.*=",,`
 NEW_VERSION=`cat .temp | grep new_version | sed -r s,"^.*=",,`
 
+git tag "v${NEW_VERSION}"
+
 git add .bumpversion.cfg
 git add custom_components.json
 git add midea.py
 git add setup.py
 
 git commit -m "[ci skip] Version Changed from ${CURRENT_VERSION} -> ${NEW_VERSION}"
-git remote add origin-build https://${GH_TOKEN}@github.com/NeoAcheron/midea-ac-py.git > /dev/null 2>&1
-git push --quiet --set-upstream origin-build master 
+git remote add build https://${GH_TOKEN}@github.com/NeoAcheron/midea-ac-py.git > /dev/null 2>&1
+git push --quiet --tags --set-upstream build master 
