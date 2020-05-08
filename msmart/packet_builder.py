aa77323794c5aec02fb8edb4ed83a5f1a2c7924e
mac_zhou <mac.zfl@gmail.com>
2020-05-08 12:11:24 +0800

    change name msmart, no midea yet

---
+++ msmart/packet_builder.py
@@ -0,0 +1,56 @@
+
+from msmart.command import base_command
+from msmart.security import security
+
+VERSION = '0.1.8'
+
+
+class packet_builder:
+
+    def __init__(self, device_id):
+        self.command = None
+        self.security = security()
+        # aa20ac00000000000003418100ff03ff000200000000000000000000000006f274
+        # Init the packet with the header data.
+        self.packet = bytearray([
+            # 2 bytes - StaicHeader
+            0x5a, 0x5a,
+            # 2 bytes - mMessageType
+            0x01, 0x11,
+            # 1 bytes - PacketLenght
+            0x68,
+            # 3 bytes
+            0x00, 0x20, 0x00,
+            # 4 bytes - MessageId 
+            0x00, 0x00, 0x00, 0x00,
+            # 8 bytes - Date&Time
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            # 6 bytes - mDeviceID
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            # 14 bytes 
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        ])
+        self.packet[20:26] = bytearray.fromhex(device_id)
+
+    def set_command(self, command: base_command):
+        self.command = command.finalize()
+
+    def finalize(self):
+        # Add cheksum
+        self.command.append(self.checksum(self.command[1:]))
+        if(__debug__):
+            print("Finalize request data: {}".format(self.command.hex()))
+        # Append the command data(48 bytes) to the packet
+        self.packet.extend(self.security.aes_encrypt(self.command)[:48])
+        # Set the packet length in the packet!
+        self.packet[0x04] = len(self.packet) + 16
+        # Append a basic checksum data(16 bytes) to the packet
+        self.packet.extend(self.encode32(self.packet))
+        return self.packet
+
+    def encode32(self, data):
+        # 16 bytes encode32
+        return self.security.encode32_data(data)
+
+    def checksum(self, data):
+        return 255 - sum(data) % 256 + 1
---
+++ msmart/security.py
@@ -0,0 +1,70 @@
+
+import hashlib
+import urllib
+from urllib.parse import urlparse
+from urllib.request import unquote
+from Crypto.Cipher import AES
+from base64 import b64decode
+from base64 import b64encode
+
+VERSION = '0.1.8'
+appKey = '434a209a5ce141c3b726de067835d7f0'
+signKey = 'xhdiwjnchekd4d512chdjx5d8e4c394D2D7S'
+
+class security:
+
+    def __init__(self):
+        self.appKey = appKey
+        self.signKey = signKey
+        self.blockSize = 16
+        self.encKey = self.enc_key()
+        self.dynamicKey = self.dynamic_key()
+         
+
+    def aes_decrypt(self, raw):
+        cipher = AES.new(self.encKey, AES.MODE_ECB)
+        decrypted = cipher.decrypt(bytes(raw))
+
+        # Remove the padding
+        decrypted = self._unpad(decrypted)   
+
+        return decrypted
+
+    def aes_encrypt(self, raw):
+        # Make sure to pad the data
+        self._pad(raw)
+        
+        cipher = AES.new(self.encKey, AES.MODE_ECB)
+        encrypted = cipher.encrypt(bytes(raw))
+            
+        return encrypted
+
+    def _pad(self, s):
+        pad = self.blockSize - (len(s) % self.blockSize)
+        s.extend([pad] * pad)
+       
+    def _unpad(self, s):
+        return s[:-s[-1]]
+
+    def enc_key(self):
+        m = hashlib.md5()
+        # Hash the signKey
+        m.update(self.signKey.encode('ascii'))
+        # Use the HEX output of the hash
+        key = bytes.fromhex(m.hexdigest())
+        return key
+
+    def dynamic_key(self):
+        m = hashlib.md5()
+        # Hash the appKey
+        m.update(self.appKey.encode('ascii'))
+        # Use only half the HEX output of the hash
+        key = bytes.fromhex(m.hexdigest()[:16])
+        return key
+
+    def encode32_data(self, raw):
+        combine = raw + signKey.encode()
+        m = hashlib.md5()
+        m.update(combine)
+        key = bytes.fromhex(m.hexdigest())
+        return key
