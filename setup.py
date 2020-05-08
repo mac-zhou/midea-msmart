9620b97279a869d17478594a61ecac53dc4d9901
mac_zhou <mac.zfl@gmail.com>
2020-05-08 09:28:19 +0800

    new name and change auther

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='midea',  
-     version='0.1.7',
-     author="NeoAcheron",
-     author_email="master@neoacheron.com",
-     description="A library to control Midea appliances via the cloud API",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/NeoAcheron/midea-ac-py",
-     packages=setuptools.find_packages(),
-     classifiers=[
+    name='midea-msmart',
+    version='0.1.7',
+    author="mac_zhou",
+    author_email="mac.zfl@gmail.com",
+    description="A library to control Midea appliances via the Local area network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mac-zhou/midea-msmart-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
-     ],
- )
+    ],
+)
