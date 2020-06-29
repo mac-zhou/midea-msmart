2f7150ab1fb06d15f1b64197303fc351d8a5108b
mac_zhou <mac.zfl@gmail.com>
2020-06-29 11:01:22 +0800

    update

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,14 +3,14 @@
 echo Currently on branch `git branch`
 
 bumpversion patch --no-tag --allow-dirty --no-commit --list > .temp
-CURRENT_VERSION=`cat .temp | grep current_version | sed -r s,"^.*=",,`
-NEW_VERSION=`cat .temp | grep new_version | sed -r s,"^.*=",,`
+CURRENT_VERSION=`cat .temp | grep current_version | sed s,"^.*=",,`
+NEW_VERSION=`cat .temp | grep new_version | sed s,"^.*=",,`
 
 git add .bumpversion.cfg
 git add custom_components.json
-git add midea.py
+#git add midea.py
 git add setup.py
-git add midea/*.py
+git add msmart/*.py
 
 git commit -m "Version Changed from ${CURRENT_VERSION} -> ${NEW_VERSION}"
 git tag "v${NEW_VERSION}"
