733d25a5456bee3d625f400155a4b75f20f14f13
Colin Kuebler <kueblc@gmail.com>
2020-07-26 17:24:39 -0400

    Maintain an open TCP socket, rather than creating a new one for every communication This is necessary for session based authentication

--- msmart/lan.py
+++ msmart/lan.py
@@ -16,34 +16,39 @@ class lan:
         self.device_port = 6444
         self.security = security()
         self._retries = 0
+        self._socket = None
+
+    def _connect(self):
+        if self._socket == None:
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._socket.settimeout(8)
+            try:
+                self._socket.connect((self.device_ip, self.device_port))
+            except:
+                self._socket = None
 
     def request(self, message):
         # Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(8)
+        self._connect()
 
         try:
-            # Connect the Device
-            device_address = (self.device_ip, self.device_port)
-            sock.connect(device_address)
-
             # Send data
             _LOGGER.debug("Sending to {}:{} {}".format(
                 self.device_ip, self.device_port, message.hex()))
-            sock.sendall(message)
+            self._socket.send(message)
 
             # Received data
-            response = sock.recv(512)
+            response = self._socket.recv(512)
         except socket.error:
             _LOGGER.info("Couldn't connect with Device {}:{}".format(
                 self.device_ip, self.device_port))
+            self._socket = None
             return bytearray(0)
         except socket.timeout:
             _LOGGER.info("Connect the Device %s:%s TimeOut for 8s. don't care about a small amount of this. if many maybe not support".format(
                 self.device_ip, self.device_port))
+            self._socket = None
             return bytearray(0)
-        finally:
-            sock.close()
         _LOGGER.debug("Received from {}:{} {}".format(
             self.device_ip, self.device_port, message.hex()))
         return response
