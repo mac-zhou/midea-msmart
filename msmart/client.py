1b38d8d19a1d4e615ec58a44935294bfa28feba7
Mac_Zhou <mac_zhou@live.com>
2021-08-13 14:43:21 +0800

    Version 0.1.29

--- msmart/client.py
+++ msmart/client.py
@@ -10,7 +10,7 @@ from msmart.device import air_conditioning_device
 from msmart.device import dehumidifier_device
 from msmart.device import unknown_device
 
-VERSION = '0.1.28'
+VERSION = '0.1.29'
 
 DEVICE_TYPES = {
     0xAC: air_conditioning_device,
--- msmart/command.py
+++ msmart/command.py
@@ -3,7 +3,7 @@ import logging
 import datetime
 import msmart.crc8 as crc8
 
-VERSION = '0.1.28'
+VERSION = '0.1.29'
 
 _LOGGER = logging.getLogger(__name__)
 
