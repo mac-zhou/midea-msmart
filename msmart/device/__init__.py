67ffc4d09f6cb04d3b5bf2ebe4a0bef331ebd6c8
Mac_Zhou <mac_zhou@live.com>
2022-03-03 13:08:10 +0800

    add 0xdb

--- msmart/device/__init__.py
+++ msmart/device/__init__.py
@@ -2,6 +2,7 @@
 
 # 0xAC - Air Conditioner
 from .AC.appliance import air_conditioning
+# 0xDB - Front Load Washer https://www.midea.com/sg/washer
+from .DB.appliance import front_load_washer
 
 VERSION = '0.2.2'
-
