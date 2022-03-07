9b7e418880cc16c9c33d8104b26b591c718afb7e
Mac_Zhou <mac_zhou@live.com>
2022-03-07 10:14:15 +0800

    delete set_command & add CMD_TYPE_REPORT

--- msmart/device/DB/command.py
+++ msmart/device/DB/command.py
@@ -3,7 +3,7 @@ import logging
 import datetime
 import msmart.crc8 as crc8
 from msmart.utils import getBit, getBits
-from msmart.const import CMD_TYPE_CONTROL, CMD_TYPE_QUERRY
+from msmart.const import CMD_TYPE_CONTROL, CMD_TYPE_QUERRY, CMD_TYPE_REPORT
 
 VERSION = '0.2.2'
 
@@ -74,140 +74,16 @@ class base_command:
         _LOGGER.debug("Finalize request data: {}".format(self.data.hex()))
         return self.data
 
-
-class set_command(base_command):
-
-    def __init__(self, device_type):
-        base_command.__init__(self, device_type)
-        self.data[0x01] = 0x23
-        self.data[0x09] = 0x02
-        # Set up Mode
-        self.data[0x0a] = 0x40
-        # prompt_tone
-        self.data[0x0b] = 0x40
-        self.data.extend(bytearray([0x00, 0x00, 0x00]))
-
-    @property
-    def prompt_tone(self):
-        return self.data[0x0b] & 0x42
-
-    @prompt_tone.setter
-    def prompt_tone(self, feedback_anabled: bool):
-        self.data[0x0b] &= ~ 0x42  # Clear the audible bits
-        self.data[0x0b] |= 0x42 if feedback_anabled else 0
-
-    @property
-    def power_state(self):
-        return self.data[0x0b] & 0x01
-
-    @power_state.setter
-    def power_state(self, state: bool):
-        self.data[0x0b] &= ~ 0x01  # Clear the power bit
-        self.data[0x0b] |= 0x01 if state else 0
-
-    @property
-    def target_temperature(self):
-        return self.data[0x0c] & 0x1f
-
-    @target_temperature.setter
-    def target_temperature(self, temperature_celsius: float):
-        # Clear the temperature bits.
-        self.data[0x0c] &= ~ 0x0f
-        # Clear the temperature bits, except the 0.5 bit, which will be set properly in all cases
-        self.data[0x0c] |= (int(temperature_celsius) & 0xf)
-        # set the +0.5 bit
-        self.temperature_dot5 = (int(round(temperature_celsius*2)) % 2 != 0)
-
-    @property
-    def operational_mode(self):
-        return (self.data[0x0c] & 0xe0) >> 5
-
-    @operational_mode.setter
-    def operational_mode(self, mode: int):
-        self.data[0x0c] &= ~ 0xe0  # Clear the mode bit
-        self.data[0x0c] |= (mode << 5) & 0xe0
-
-    @property
-    def fan_speed(self):
-        return self.data[0x0d]
-
-    @fan_speed.setter
-    def fan_speed(self, speed: int):
-        self.data[0x0d] = speed
-
-    @property
-    def eco_mode(self):
-        return self.data[0x13] > 0
-
-    @eco_mode.setter
-    def eco_mode(self, eco_mode_enabled: bool):
-        self.data[0x13] = 0xFF if eco_mode_enabled else 0
-
-    @property
-    def swing_mode(self):
-        return self.data[0x11]
-
-    @swing_mode.setter
-    def swing_mode(self, mode: int):
-        self.data[0x11] = 0x30  # Clear the mode bit
-        self.data[0x11] |= mode & 0x3f
-
-    @property
-    def turbo_mode(self):
-        return self.data[0x14] > 0
-
-    @turbo_mode.setter
-    def turbo_mode(self, turbo_mode_enabled: bool):
-        if (turbo_mode_enabled):
-            self.data[0x14] |= 0x02
-        else:
-            self.data[0x14] &= (~0x02)
-
-    @property
-    def screen_display(self):
-        return self.data[0x14] & 0x10 > 0
-
-    @screen_display.setter
-    def screen_display(self, screen_display_enabled: bool):
-        # the LED lights on the AC. these display temperature and are often too bright during nights
-        if screen_display_enabled:
-            self.data[0x14] |= 0x10
-        else:
-            self.data[0x14] &= (~0x10)
-
-    @property
-    def temperature_dot5(self):
-        return self.data[0x0c] & 0x10 > 0
-
-    @temperature_dot5.setter
-    def temperature_dot5(self, temperature_dot5_enabled: bool):
-        # add 0.5C to the temperature value. not intended to be called directly. target_temperature setter calls this if needed
-        if temperature_dot5_enabled:
-            self.data[0x0c] |= 0x10
-        else:
-            self.data[0x0c] &= (~0x10)
-
-    @property
-    def fahrenheit(self):
-        # is the temperature unit fahrenheit? (celcius otherwise)
-        return self.data[0x14] & 0x04 > 0
-
-    @fahrenheit.setter
-    def fahrenheit(self, fahrenheit_enabled: bool):
-        # set the unit to fahrenheit from celcius
-        if fahrenheit_enabled:
-            self.data[0x14] |= 0x04
-        else:
-            self.data[0x14] &= (~0x04)
-
-
 class appliance_response:
 
     def __init__(self, data: bytearray):
         self.header = data[:0xa]
         self.data = data[0xa:-1]
         self.message_type = data[0x09]
-        _LOGGER.info("Appliance response type: {} data: {}".format(self.message_type, self.data.hex()))
+        self.update = True
+        if self.message_type ==CMD_TYPE_REPORT and self.data[0] != CMD_TYPE_REPORT:
+            self.update = False
+        _LOGGER.info("Appliance response type: {} update:{} data: {}".format(self.message_type, self.update, self.data.hex()))
 
     # Byte 0x01
     @property
