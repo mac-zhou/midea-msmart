283e2027950de241a6ced37e63041c7e890e4b21
Tucker Kern <mill1000@users.noreply.github.com>
2023-08-29 20:37:00 +0000

    Increase timeout for slower connections

--- msmart/cloud.py
+++ msmart/cloud.py
@@ -96,7 +96,7 @@ class Cloud:
             while retries > 0:
                 try:
                     # Post request and handle bad status code
-                    r = await client.post(url, headers=headers, content=contents)
+                    r = await client.post(url, headers=headers, content=contents, timeout=10.0)
                     r.raise_for_status()
 
                     # Parse the response
